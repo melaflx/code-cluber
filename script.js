613ad2592c9f67c592bee13d7e0fa509e3680b6a
melaflx <42240042+melaflx@users.noreply.github.com>
2018-08-09 12:48:05 +0100

    My first commit 'Yay'

---
+++ script.js
@@ -0,0 +1,35 @@
+// Add your code below...
+const myName = "Melanie"
+const myLastname = "Felix"
+const myFullName = myName+ " "+myLastname
+
+const number1 = 4
+const number2 = 2
+const sumNumbers = number1+ number2
+
+//alert("Hi "+myFullName+ ".Your favourite number is: " + sumNumbers)
+
+function flipCoin(threshold) {
+	const myRandomNumber = Math.random()
+	if (myRandomNumber > threshold) {
+		alert("You won, " + myName)
+		alert("You really won!")
+	} else {
+		alert("You lost " + myName)
+		alert("Loser!")
+	}
+
+}
+
+function resizeButton() {
+	const newRadius = Math.random() *20;
+	$('.button').css('border-radius',newRadius +'px');
+
+}
+
+function slideImage() {
+	$('.myProfile').slideUp();
+	$('.myProfile').slideDown();
+}
+
+//flipCoin()
